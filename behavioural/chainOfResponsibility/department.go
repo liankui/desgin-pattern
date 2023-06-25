667e0ac84020506230bce8628502dfa310cbe190
liankui <yuekewin@gmail.com>
2023-06-25 14:36:00 +0800

    refactor: chainOfResponsibility

--- behavioural/chainOfResponsibility/department.go
+++ behavioural/chainOfResponsibility/department.go
@@ -1,4 +1,6 @@
-package chainOfResponsibility
+package main
+
+import "fmt"
 
 /* https://golangbyexample.com/chain-of-responsibility-design-pattern-in-golang/
 责任链设计模式是一种行为设计模式。它允许您创建一条请求处理程序链。对于每个传入的请求，它通过链和每个处理程序传递:
@@ -20,3 +22,110 @@ type department interface {
 	execute(*patient)
 	setNext(department)
 }
+
+type reception struct {
+	next department
+}
+
+func (r *reception) execute(p *patient) {
+	if p.registrationDone {
+		fmt.Println("Patient registration already done")
+		r.next.execute(p)
+		return
+	}
+	fmt.Println("Reception registering patient")
+	p.registrationDone = true
+	r.next.execute(p)
+}
+
+func (r *reception) setNext(next department) {
+	r.next = next
+}
+
+type doctor struct {
+	next department
+}
+
+func (d *doctor) execute(p *patient) {
+	if p.doctorCheckUpDone {
+		fmt.Println("Doctor checkup already done")
+		d.next.execute(p)
+		return
+	}
+	fmt.Println("Doctor checking patient")
+	p.doctorCheckUpDone = true
+	d.next.execute(p)
+}
+
+func (d *doctor) setNext(next department) {
+	d.next = next
+}
+
+type medical struct {
+	next department
+}
+
+func (m *medical) execute(p *patient) {
+	if p.medicineDone {
+		fmt.Println("Medicine already given to patient")
+		m.next.execute(p)
+		return
+	}
+	fmt.Println("Medical giving medicine to patient")
+	p.medicineDone = true
+	m.next.execute(p)
+}
+
+func (m *medical) setNext(next department) {
+	m.next = next
+}
+
+type cashier struct {
+	next department
+}
+
+func (c *cashier) execute(p *patient) {
+	if p.paymentDone {
+		fmt.Println("Payment Done")
+	}
+	fmt.Println("Cashier getting money from patient patient")
+}
+
+func (c *cashier) setNext(next department) {
+	c.next = next
+}
+
+type patient struct {
+	name              string
+	registrationDone  bool
+	doctorCheckUpDone bool
+	medicineDone      bool
+	paymentDone       bool
+}
+
+func main() {
+	cashier := &cashier{}
+
+	// Set next for medical department
+	medical := &medical{}
+	medical.setNext(cashier)
+
+	// Set next for doctor department
+	doctor := &doctor{}
+	doctor.setNext(medical)
+
+	// Set next for reception department
+	reception := &reception{}
+	reception.setNext(doctor)
+
+	patient := &patient{name: "abc"}
+	// Patient visiting
+	reception.execute(patient)
+}
+
+/* Output:
+Reception registering patient
+Doctor checking patient
+Medical giving medicine to patient
+Cashier getting money from patient patient
+*/
