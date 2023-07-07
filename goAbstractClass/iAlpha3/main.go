cccf1e93db66c704ed3cbd09b4d8210b9d9dd18a
liankui <yuekewin@gmail.com>
2023-07-07 14:33:37 +0800

    feat: Abstract Class in GO: Complete Guide

---
+++ goAbstractClass/iAlpha3/main.go
@@ -0,0 +1,45 @@
+package main
+
+import "fmt"
+
+// Abstract Interface
+type iAlpha interface {
+	work()
+	common(iAlpha)
+}
+
+// Abstract Concrete Type
+type alpha struct {
+	name string
+}
+
+func (a *alpha) common(i iAlpha) {
+	fmt.Println("common called")
+	i.work()
+}
+
+// Implementing Type
+type beta struct {
+	alpha
+}
+
+func (b *beta) work() {
+	fmt.Println("work called")
+	fmt.Printf("Name is %s\n", b.name)
+}
+
+func main() {
+	a := alpha{
+		name: "test",
+	}
+	b := &beta{
+		alpha: a,
+	}
+	b.common(b)
+}
+
+/* Output:
+common called
+work called
+Name is test
+*/
