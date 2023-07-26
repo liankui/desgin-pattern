cdb6ab7c31b9a064a30cf50713502342f42388df
liankui <yuekewin@gmail.com>
2023-07-26 10:38:50 +0800

    feat: add linkedList TODO

--- goDataStructure/linkedList/linkedList/linkedList.go
+++ goDataStructure/linkedList/linkedList/linkedList.go
@@ -1,3 +1,46 @@
 package linkedList
 
 // https://golangbyexample.com/singly-linked-list-in-golang/
+
+type element struct {
+	value string
+	next  *element
+}
+
+type singleList struct {
+	len  int
+	head *element
+}
+
+func newSingleList() *singleList {
+	return &singleList{}
+}
+
+func (s *singleList) AddFront(value string) {
+	ele := &element{value: value}
+
+	if s.head == nil {
+		s.head = ele
+	} else {
+		ele.next = s.head
+		s.head = ele
+	}
+
+	s.len++
+}
+
+func (s *singleList) AddBack(value string) {
+	ele := &element{value: value}
+
+	if s.head == nil {
+		s.head = ele
+	} else {
+		current := s.head
+		for current.next != nil {
+			current = current.next
+		}
+		current.next = ele
+	}
+
+	s.len++
+}
