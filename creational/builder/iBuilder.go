36725558e83dfd22568f8259238c6c2374e6f7eb
Eric Yue <yuekewin@gmail.com>
2024-07-12 11:01:53 +0800

    udpate builder, add desc

--- creational/builder/iBuilder.go
+++ creational/builder/iBuilder.go
@@ -10,6 +10,12 @@ import (
 1.逐步构建一个复杂对象。
 2.将对象的构建过程与表现形式分离。
 3.相同的构建过程能够创建对象的不同表现形式。
+
+模块
+1.	Product（产品）：这是正在构建的复杂对象（包含 PartA、PartB 部分）。
+2.	Builder 接口：定义了构建产品各部分的方法。
+3.	ConcreteBuilder（具体建造者）：实现了 Builder 接口，负责构建和组装产品的各个部分。
+4.	Director（指导者）：使用 Builder 来构建产品，封装了构建过程。
 */
 
 type Product struct {
